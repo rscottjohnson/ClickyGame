9f544983f85797b173bea08783ec1950b8e81eae
Scott Johnson <sjohnson.wvu@gmail.com>
2018-09-14 22:09:59 -0400

    Updated ImageCard component

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -4,7 +4,7 @@ import "./ImageCard.css";
 const ImageCard = props => (
   <div className="card" onClick={() => props.clickCount(props.id)}>
     <div className="img-container">
-      <img class="img-thumbnail" alt={props.name} src={props.image} />
+      <img className="img-thumbnail" alt={props.name} src={props.image} />
     </div>
   </div>
 );
